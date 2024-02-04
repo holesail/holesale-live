c9591e4c719b2eaef5b4fe637a3a0ca8bf1b3003
Codesamp-Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2024-02-05 03:27:16 +0530

    files updated

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Navbar } from "../components/Navbar";
 
 export default function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export function Navbar() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
