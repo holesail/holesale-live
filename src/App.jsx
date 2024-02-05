7f1c9d2f8c8b0de4466684eda8de627919be7270
Codesamp-Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2024-02-05 12:36:05 +0530

    Tailwind added along with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { HomeSection } from "./HomeSection";
 import { Navbar } from "./components/Navbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <HomeSection />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 export function Navbar() {
+  const links = [
+    { link: "About Us" },
+    { link: "Contact Us" },
+    { link: "Join Us" },
+  ];
+
   return (
     <>
-      <h1>Hello</h1>
+      <nav className="p-[2rem] flex justify-between border-b-[1px] border-[#2a2a2a]">
+        <img className="web--logo" src="./vite.svg"></img>
+        <ul className="flex gap-[30px] text-[#777]">
+          {links.map((link, index) => (
+            <li key={index} className="hover:text-[#eee] transition-[400]">
+              <a href="/">{link.link}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </>
   );
 }
